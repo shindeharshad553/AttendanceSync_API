38803dd0c8e6ba88ead3d1d542e1b87e788988ae
shindeharshad553 <shindeharshad553@gmail.com>
2025-03-11 00:36:49 +0530

    updated code

--- src/main/java/com/attendancesync/api/AttendanceSyncTrackApplication.java
+++ src/main/java/com/attendancesync/api/AttendanceSyncTrackApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableAutoConfiguration
 //@ComponentScan
 @SpringBootApplication
-public class SamsTrackApplication {
+public class AttendanceSyncTrackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SamsTrackApplication.class, args);
+		SpringApplication.run(AttendanceSyncTrackApplication.class, args);
 	}
 
 }
--- src/test/java/com/attendancesync/api/AttendanceSyncTrackApplicationTests.java
+++ src/test/java/com/attendancesync/api/AttendanceSyncTrackApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jbk.api;
+package com.attendancesync.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SamsTrackApplicationTests {
+class AttendanceSyncTrackApplicationTests {
 
 	@Test
 	void contextLoads() {
